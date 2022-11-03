f7852334e73615cae09973ba6d089f0668ac5c36
achang <alexander.h.chang@gatech.edu>
2022-11-03 16:38:02 -0400

    setup for set snakey config across multiple motors

--- matlab/motor_intfcs/test/configure_snakey_dxl_motor.m
+++ matlab/motor_intfcs/test/configure_snakey_dxl_motor.m
@@ -6,13 +6,14 @@
 % 
 
 % [0] == Script parameter(s)
-PORT_NAME = '/dev/ttyUSB0';
+PORT_NAME = '/dev/ttyUSB1';
 PORT_BAUD = 1000000;
-
-MOTOR_ID = 12;
 
 MOTOR_OP_MODE = 4;  % 3 = Default Position Control Mode; 4 = Extended Position Control Mode
 MOTOR_HOMING_OFFSET = -180*pi/180;%-180*pi/180;  % rad
+
+MOTOR_IDS = 1:12;
+% MOTOR_IDS = 12;
 
 
 % [1] == Script setup
@@ -32,94 +33,102 @@ fprintf('Opening port: %s at baud: %d.... \n', PORT_NAME, PORT_BAUD);
 openPortResult = dxlio.openPort( PORT_NAME, PORT_BAUD );
 fprintf('Open port success: %d.\n\n', openPortResult);
 
-%   Ping motor
-fprintf('Pinging target motor ...\n');
-ping_result = dxlio.pingGetModelNum( MOTOR_ID );
-if ( ~ping_result )
-%   fprintf('\nPing result -> no response!');
-  fprintf('[not found] Motor ID: %d -> no response.\n\n', MOTOR_ID);
-else
-%   fprintf('Ping result -> Model number: %d, for Motor ID: %d.\n\n', ping_result, MOTOR_ID);
-  fprintf('[FOUND] Motor ID: %d -> Model number: %d (%s).\n\n', MOTOR_ID, ping_result, DXL_IO_Impl.MODEL_NUM2NAME(ping_result));
+%   Ping motors
+fprintf('Pinging target motors ...\n');
+for ii = 1:length(MOTOR_IDS)
+  ping_result = dxlio.pingGetModelNum( MOTOR_IDS(ii) );
+  if ( ~ping_result )
+  %   fprintf('\nPing result -> no response!');
+    fprintf('[not found] Motor ID: %d -> no response.\n\n', MOTOR_IDS(ii));
+  else
+  %   fprintf('Ping result -> Model number: %d, for Motor ID: %d.\n\n', ping_result, MOTOR_ID);
+    fprintf('[FOUND] Motor ID: %d -> Model number: %d (%s).\n\n', MOTOR_IDS(ii), ping_result, DXL_IO_Impl.MODEL_NUM2NAME(ping_result));
+  end
+  pause(1);
 end
-pause(1);
 
 
 % [3] == Set EEPROM motor properties 
-%     Disable motor torque
-torque_state = 0;
-fprintf('Disabling torque: %d, for motor ID: %d.\n\n', torque_state, MOTOR_ID);
-dxlio.set_torque_enable( MOTOR_ID, torque_state );
-pause(1);
+for ii = 1:length(MOTOR_IDS)
+  %     Disable motor torque
+  torque_state = 0;
+  fprintf('Disabling torque: %d, for motor ID: %d.\n\n', torque_state, MOTOR_IDS(ii));
+  dxlio.set_torque_enable( MOTOR_IDS(ii), torque_state );
+  pause(1);
+  
+  %   Set operating mode to: Extended Position Control Mode
+  fprintf('[Motor ID: %d] Setting Operating Mode: %d.\n\n', MOTOR_IDS(ii), MOTOR_OP_MODE);
+  dxlio.set_operating_mode(MOTOR_IDS(ii), MOTOR_OP_MODE);
+  pause(1);
+  
+  %     Set homing offset
+  fprintf('Configuring homing offset: %.4f rad, for motor ID: %d.\n\n', MOTOR_HOMING_OFFSET, MOTOR_IDS(ii));
+  dxlio.set_homing_offset( MOTOR_IDS(ii), MOTOR_HOMING_OFFSET );
+  pause(1);
+  
+  %   Print-out EEPROM motor configuration
+  print_info = true;
+  motor_status_str = dxlio.get_motor_eeprom_state( MOTOR_IDS(ii), print_info );
+  pause(1);
+end
 
-%   Set operating mode to: Extended Position Control Mode
-fprintf('[Motor ID: %d] Setting Operating Mode: %d.\n\n', MOTOR_ID, MOTOR_OP_MODE);
-dxlio.set_operating_mode(MOTOR_ID, MOTOR_OP_MODE);
-pause(1);
 
-%     Set homing offset
-fprintf('Configuring homing offset: %.4f rad, for motor ID: %d.\n\n', MOTOR_HOMING_OFFSET, MOTOR_ID);
-dxlio.set_homing_offset( MOTOR_ID, MOTOR_HOMING_OFFSET );
-pause(1);
-
-%   Print-out EEPROM motor configuration
-print_info = true;
-motor_status_str = dxlio.get_motor_eeprom_state( MOTOR_ID, print_info );
-pause(1);
-
-
-% [4] == Set EEPROM motor properties 
-%   Enable motor torque
-torque_state = 1;
-fprintf('[Motor ID: %d] Enabling torque: %d.\n\n', MOTOR_ID, torque_state);
-dxlio.set_torque_enable( MOTOR_ID, torque_state );
-pause(1);
-
-%   Command motor position: 0 deg
-goal_pos = (0)*pi/180;  % rad
-input(sprintf('Press <Enter> to travel to %.2f deg.\n', goal_pos*180/pi));
-fprintf('[Motor ID: %d] Commanding goal position: %.2f deg.\n\n', MOTOR_ID, goal_pos*180/pi);
-dxlio.set_goal_position( MOTOR_ID, goal_pos );
-pause(2);
-
-%     Read present motor position
-[ cur_motor_pos ] = dxlio.get_present_position( MOTOR_ID );
-fprintf('Retrieved motor position: %.4f rad (%.3f deg), motor ID: %d.\n', cur_motor_pos, cur_motor_pos*180/pi, MOTOR_ID);
-pause(1);
-
-%   Command motor position: 0 deg
-goal_pos = (90)*pi/180;  % rad
-input(sprintf('Press <Enter> to travel to %.2f deg.\n', goal_pos*180/pi));
-fprintf('[Motor ID: %d] Commanding goal position: %.2f deg.\n\n', MOTOR_ID, goal_pos*180/pi);
-dxlio.set_goal_position( MOTOR_ID, goal_pos );
-pause(2);
-
-%     Read present motor position
-[ cur_motor_pos ] = dxlio.get_present_position( MOTOR_ID );
-fprintf('Retrieved motor position: %.4f rad (%.3f deg), motor ID: %d.\n', cur_motor_pos, cur_motor_pos*180/pi, MOTOR_ID);
-pause(1);
-
-%   Command motor position: 0 deg
-goal_pos = (-90)*pi/180;  % rad
-input(sprintf('Press <Enter> to travel to %.2f deg.\n', goal_pos*180/pi));
-fprintf('[Motor ID: %d] Commanding goal position: %.2f deg.\n\n', MOTOR_ID, goal_pos*180/pi);
-dxlio.set_goal_position( MOTOR_ID, goal_pos );
-pause(2);
-
-%     Read present motor position
-[ cur_motor_pos ] = dxlio.get_present_position( MOTOR_ID );
-fprintf('Retrieved motor position: %.4f rad (%.3f deg), motor ID: %d.\n', cur_motor_pos, cur_motor_pos*180/pi, MOTOR_ID);
-pause(1);
+% [4] == Set motor zero position
+for ii = 1:length(MOTOR_IDS)
+  %   Enable motor torque
+  torque_state = 1;
+  fprintf('[Motor ID: %d] Enabling torque: %d.\n\n', MOTOR_IDS(ii), torque_state);
+  dxlio.set_torque_enable( MOTOR_IDS(ii), torque_state );
+  pause(1);
+  
+  %   Command motor position: 0 deg
+  goal_pos = (0)*pi/180;  % rad
+  input(sprintf('Press <Enter> to travel to %.2f deg.\n', goal_pos*180/pi));
+  fprintf('[Motor ID: %d] Commanding goal position: %.2f deg.\n\n', MOTOR_IDS(ii), goal_pos*180/pi);
+  dxlio.set_goal_position( MOTOR_IDS(ii), goal_pos );
+  pause(2);
+  
+  %     Read present motor position
+  [ cur_motor_pos ] = dxlio.get_present_position( MOTOR_IDS(ii) );
+  fprintf('Retrieved motor position: %.4f rad (%.3f deg), motor ID: %d.\n', cur_motor_pos, cur_motor_pos*180/pi, MOTOR_IDS(ii));
+  pause(1);
+  
+%   %   Command motor position: 90 deg
+%   goal_pos = (90)*pi/180;  % rad
+%   input(sprintf('Press <Enter> to travel to %.2f deg.\n', goal_pos*180/pi));
+%   fprintf('[Motor ID: %d] Commanding goal position: %.2f deg.\n\n', MOTOR_ID, goal_pos*180/pi);
+%   dxlio.set_goal_position( MOTOR_ID, goal_pos );
+%   pause(2);
+%   
+%   %     Read present motor position
+%   [ cur_motor_pos ] = dxlio.get_present_position( MOTOR_ID );
+%   fprintf('Retrieved motor position: %.4f rad (%.3f deg), motor ID: %d.\n', cur_motor_pos, cur_motor_pos*180/pi, MOTOR_ID);
+%   pause(1);
+%   
+%   %   Command motor position: -90 deg
+%   goal_pos = (-90)*pi/180;  % rad
+%   input(sprintf('Press <Enter> to travel to %.2f deg.\n', goal_pos*180/pi));
+%   fprintf('[Motor ID: %d] Commanding goal position: %.2f deg.\n\n', MOTOR_ID, goal_pos*180/pi);
+%   dxlio.set_goal_position( MOTOR_ID, goal_pos );
+%   pause(2);
+%   
+%   %     Read present motor position
+%   [ cur_motor_pos ] = dxlio.get_present_position( MOTOR_ID );
+%   fprintf('Retrieved motor position: %.4f rad (%.3f deg), motor ID: %d.\n', cur_motor_pos, cur_motor_pos*180/pi, MOTOR_ID);
+%   pause(1);
+end
 
 
 % [5] == Clean-up
-%   Disable motor torque
-torque_state = 0;
-fprintf('[Motor ID: %d] Disabling torque: %d.\n\n', MOTOR_ID, torque_state);
-dxlio.set_torque_enable( MOTOR_ID, torque_state );
-pause(1);
+for ii = 1:length(MOTOR_IDS)
+  %   Disable motor torque
+  torque_state = 0;
+  fprintf('[Motor ID: %d] Disabling torque: %d.\n\n', MOTOR_IDS(ii), torque_state);
+  dxlio.set_torque_enable( MOTOR_IDS(ii), torque_state );
+  pause(1);
+end
 
-%   Clean-up
+%   Close port, unload library
 fprintf('Closing DXL port: %s.\n', PORT_NAME);
 dxlio.closePort();
 fprintf('Unloading DXL library.\n');
